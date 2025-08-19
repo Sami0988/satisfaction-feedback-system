55218dc32f2e67f0227e45785eac3ea8d43d782c
samuel <zsami9017@gmail.com>
2025-08-19 05:06:56 -0700

    SWAGGER SETUP

---
+++ backend/app/Http/Controllers/Swagger/SwaggerInfo.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers\Swagger;
+
+use OpenApi\Annotations as OA;
+
+/**
+ * @OA\Info(
+ *      version="1.0.0",
+ *      title="Customer Satisfaction API",
+ *      description="API documentation for Customer Satisfaction System",
+ *      @OA\Contact(
+ *          email="youremail@example.com"
+ *      )
+ * )
+ */
+class SwaggerInfo
+{
+    // This class is just for Swagger annotations
+}
---
+++ backend/app/Http/Controllers/UserController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+
+class UserController extends Controller
+{
+    /**
+     * @OA\Get(
+     *     path="/api/users",
+     *     tags={"Users"},
+     *     summary="Get a list of all users",
+     *     @OA\Response(
+     *         response=200,
+     *         description="A list of users"
+     *     )
+     * )
+     */
+    public function index()
+    {
+        return User::all();
+    }
+}
