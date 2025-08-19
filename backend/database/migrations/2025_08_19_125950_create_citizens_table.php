fc34b7f145e96dc0c7480c36c0af448badbc129b
samuel <zsami9017@gmail.com>
2025-08-19 06:02:29 -0700

    SCHEMA FOR EACH ENTITY

---
+++ backend/database/migrations/2025_08_19_125950_create_citizens_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+        public function up(): void
+            {
+                    Schema::create('citizens', function (Blueprint $table) {
+                        $table->uuid('id')->primary();
+                        $table->string('first_name');
+                        $table->string('phone')->unique();
+                        $table->string('password');
+                        $table->boolean('is_anonymous')->default(false);
+
+                        $table->timestamps();
+                    });
+                }       
+
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('citizens');
+    }
+};
