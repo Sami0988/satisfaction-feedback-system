3149f4a22938b17eb93d9a87c60e321303c7c0b9
samuel <zsami9017@gmail.com>
2025-08-23 04:58:38 -0700

    ADD Button

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ function App() {
   return (
     <div className="h-screen flex items-center justify-center bg-blue-100">
       <h1 className="text-4xl font-bold text-blue-800">Hello Tailwind</h1>
+      <button className="text-3xl bg-red-200">Click Me</button>
     </div>
   );
 }
