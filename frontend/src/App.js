dcae7d7921a276c5a4c5cb34cdeaee53425ee07d
samuel <zsami9017@gmail.com>
2025-08-23 05:10:21 -0700

    Button Style

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,10 @@
-
 function App() {
   return (
     <div className="h-screen flex items-center justify-center bg-blue-100">
       <h1 className="text-4xl font-bold text-blue-800">Hello Tailwind</h1>
-      <button className="text-3xl bg-red-200">Click Me</button>
+      <button className="text-3xl bg-red-200 rounded-lg ml-3 p-4">
+        Click Me
+      </button>
     </div>
   );
 }
