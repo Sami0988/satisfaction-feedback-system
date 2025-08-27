db83974669e38791169c69c62efade6cafa01f23
samuel <zsami9017@gmail.com>
2025-08-27 03:40:59 -0700

    SANCTUM SETUP

---
+++ backend/app/Http/Controllers/ServiceSelector/DepartmentController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers\ServiceSelector;  
+
+use App\Http\Controllers\Controller;
+use App\Models\Department;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+/**
+ * @OA\Tag(
+ *     name="Departments",
+ *     description="Operations about departments"
+ * )
+ */
+
+
+class DepartmentController extends Controller
+{
+    // ...
+}
