b8f7f50251dbb10893ec660cee4cf4be16b45d6c
jemu-jemu <jemilagenaa@gmail.com>
2025-09-16 00:07:07 -0700

    login page backend

--- backend/database/migrations/2025_08_27_103751_create_personal_access_tokens_table.php
+++ backend/database/migrations/2025_08_27_103751_create_personal_access_tokens_table.php
@@ -11,18 +11,16 @@ return new class extends Migration
      */
     public function up(): void
     {
-       Schema::create('personal_access_tokens', function (Blueprint $table) {
-        $table->id();
-        $table->uuid('tokenable_id'); 
-        $table->string('tokenable_type');
-        $table->string('name');
-        $table->string('token', 64)->unique();
-        $table->text('abilities')->nullable();
-        $table->timestamp('last_used_at')->nullable();
-        $table->timestamp('expires_at')->nullable()->index();
-        $table->timestamps();
-});
-
+        Schema::create('personal_access_tokens', function (Blueprint $table) {
+            $table->id();
+            $table->uuidMorphs('tokenable'); 
+            $table->string('name');
+            $table->string('token', 64)->unique();
+            $table->text('abilities')->nullable();
+            $table->timestamp('last_used_at')->nullable();
+            $table->timestamp('expires_at')->nullable()->index();
+            $table->timestamps();
+        });
     }
 
     /**
